47337d42425f337c77865d7185b339628a3f5f69
GrekovFp85 <GrekovFp85@gmail.com>
2021-11-15 21:09:36 +0200

    Разобрался. Спасибо!

--- L16/index.php
+++ L16/index.php
@@ -1,33 +1,51 @@
 <?php
 
-//Допустим, для примера это класс User, со свойствами имя и возраст;
+
+
 class User {
     public $name;
     public $age;
+    public $sex;
 }
 
-/*Напиши второй класс, назови его Builder (строитель). Создай в нем метод, который будет принимать
-в качестве аргументов имя и возраст, будет создавать нового пользователя, и возвращать его:*/
+class Builder {
 
-class Builder extends User {      //сомнение, что необходимо было создавать дочерний клас от User;
+    public static $counter = 0;
 
-    public static $number = 0;
-    public function addNewUser($name, $age) {
-        $this->name = $name;
-        $this->age = $age;
+    public function createUser($name, $age, $sex) {
 
-        self::$number++;
+        $person = new User;
+        $person->name = $name;
+        $person->age = $age;
+        $person->sex = $sex;
 
-        return 'Name: ' . $this->name . ' Age: ' . $this->age;
+        self::$counter++;
+
+        return $person;
     }
 }
 
-$person = new Builder();
 
-$person->addNewUser('Ivan',40);
-$person->addNewUser('Petr',30);
-$person->addNewUser('Karl', 20);
+$user1 = Builder::createUser('Alex', 18, 'male');
+$user2 = Builder::createUser('Olga', 21,'female');
+$user3 = Builder::createUser('Boris', 30,'male');
 
-//echo $person->addNewUser('Karl', 10);
+var_dump($user1, $user2, $user3);
 
-echo Builder::$number;
+//$list = [];
+
+//$list[] = Builder::createUser('Alex', 18, 'male');
+//$list[] = Builder::createUser('Olga', 21,'female');
+//$list[] = Builder::createUser('Boris', 30,'male');
+
+//var_dump($list);
+
+echo "\n";
+echo Builder::$counter . "\n";
+
+
+
+
+
+
+
